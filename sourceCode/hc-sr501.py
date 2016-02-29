5f3f224c065da9d4000c518d34f688bf2ffead79
yaphone <zhouyaphone@163.com>
2016-02-29 15:43:18 +0800

    edit hc-sr501

--- sourceCode/hc-sr501.py
+++ sourceCode/hc-sr501.py
@@ -4,14 +4,14 @@ import time
 #初始化
 def init():
     GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BOARD)
     GPIO.setup(18,GPIO.IN)
     GPIO.setup(21,GPIO.OUT)
     pass
  
 #蜂鸣器鸣叫函数
 def beep():
-    while GPIO.input(18):
+    while GPIO.input(26):
         GPIO.output(21,GPIO.LOW)
         time.sleep(0.5)
         GPIO.output(21,GPIO.HIGH)
@@ -21,7 +21,7 @@ def detct():
     #因为是仅仅试验，所以只让它循环运行100次
     while True:
         #如果感应器针脚输出为True，则打印信息并执行蜂鸣器函数
-        if GPIO.input(18) == True:
+        if GPIO.input(26) == True:
             print "Someone isclosing!"
             beep()
         #否则将蜂鸣器的针脚电平设置为HIGH
