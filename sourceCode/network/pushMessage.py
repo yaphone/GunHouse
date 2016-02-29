6079121e905568f1327d0a4644c850ec818feb54
yaphone <zhouyaphone@163.com>
2016-02-29 19:22:26 +0800

    edit bug

--- sourceCode/network/pushMessage.py
+++ sourceCode/network/pushMessage.py
@@ -10,13 +10,13 @@ def pushMessage(event_name, message):
     
     '''
     Args:
-        event_name: 要发送到instapush的事件名称
-        message: 要推送的信息
+        event_name: 发送到instapush对应的App名称
+        message: 要推送的信息, dict类型，{key1:value1, key2:value2},对应于instapush相应的trackers
     '''
     
     app = App(appid=App_ID, secret=App_secret)
-    app.notify(event_name=event_name, trackers={'message':message})    
+    app.notify(event_name=event_name, trackers=message)    
     
 
 if __name__ == "__main__":
-    pushMessage("hello", u"GunHouse")
+    pushMessage("hello", {"message":"hello"})
