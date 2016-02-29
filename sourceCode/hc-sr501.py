d1be7fabc5682647adef7046803ff73c0abf01e5
yaphone <zhouyaphone@163.com>
2016-02-29 15:34:37 +0800

    edit hc-sr501

--- sourceCode/hc-sr501.py
+++ sourceCode/hc-sr501.py
@@ -1,63 +1,37 @@
-#!/usr/bin/python
-#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-#|R|a|s|p|b|e|r|r|y|P|i|-|S|p|y|.|c|o|.|u|k|
-#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-#
-# pir_1.py
-# Detect movement using a PIR module
-#
-# Author : Matt Hawkins
-# Date   : 21/01/2013
- 
-# Import required Python libraries
 import RPi.GPIO as GPIO
 import time
  
-# Use BCM GPIO references
-# instead of physical pin numbers
-GPIO.setmode(GPIO.BCM)
+#初始化
+def init():
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(18,GPIO.IN)
+    GPIO.setup(21,GPIO.OUT)
+    pass
  
-# Define GPIO to use on Pi
-GPIO_PIR = 18
+#蜂鸣器鸣叫函数
+def beep():
+    while GPIO.input(18):
+        GPIO.output(21,GPIO.LOW)
+        time.sleep(0.5)
+        GPIO.output(21,GPIO.HIGH)
+        time.sleep(0.5)
+#感应器侦测函数
+def detct():
+    #因为是仅仅试验，所以只让它循环运行100次
+    while True:
+        #如果感应器针脚输出为True，则打印信息并执行蜂鸣器函数
+        if GPIO.input(18) == True:
+            print "Someone isclosing!"
+            beep()
+        #否则将蜂鸣器的针脚电平设置为HIGH
+        else:
+            GPIO.output(21,GPIO.HIGH)
+            print "Noanybody!"
+        time.sleep(2)
  
-print "PIR Module Test (CTRL-C to exit)"
- 
-# Set pin as input
-GPIO.setup(GPIO_PIR,GPIO.IN)      # Echo
- 
-Current_State  = 0
-Previous_State = 0
- 
-try:
- 
-  print "Waiting for PIR to settle ..."
- 
-  # Loop until PIR output is 0
-  while GPIO.input(GPIO_PIR)==1:
-    Current_State  = 0   
- 
-  print "  Ready"    
-     
-  # Loop until users quits with CTRL-C
-  while True :
-    
-    # Read PIR state
-    Current_State = GPIO.input(GPIO_PIR)
-    
-    if Current_State==1 and Previous_State==0:
-      # PIR is triggered
-      print "  Motion detected!"
-      # Record previous state
-      Previous_State=1
-    elif Current_State==0 and Previous_State==1:
-      # PIR has returned to ready state
-      print "  Ready"
-      Previous_State=0
-       
-    # Wait for 10 milliseconds
-    time.sleep(0.01)     
-       
-except KeyboardInterrupt:
-  print "  Quit"
-  # Reset GPIO settings
-  GPIO.cleanup()
+
+init()
+detct()
+#脚本运行完毕执行清理工作
+GPIO.cleanup()
