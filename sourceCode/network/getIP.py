44ac3ba2720436ddcdec9b8c9c4243fc232a734f
yaphone <zhouyaphone@163.com>
2016-02-29 20:08:46 +0800

    add check_network()

--- sourceCode/network/getIP.py
+++ sourceCode/network/getIP.py
@@ -1,10 +1,25 @@
 #coding=utf-8
 import re
+import time
+import urllib
 import urllib2
 import socket
 from pushMessage import pushMessage
 
-def getIP():
+
+def check_network():
+    while True:
+        try:
+            result=urllib.urlopen('http://baidu.com').read()
+            print "Network is Ready!"
+            break
+        except Exception , e:
+           print e
+           print "Network is not ready,Sleep 5s...."
+           time.sleep(5)
+    getIP()
+
+def getIP():                           #获取IP地址并发送
     try:
         lan_address = socket.gethostbyname(socket.gethostname())
         IP_address = visit("http://www.bliao.com/ip.phtml")
@@ -19,17 +34,14 @@ def getIP():
                 
     event_name = "getip"
     message = {"lan_address":lan_address, "IP_address":IP_address}
-    print message
     pushMessage(event_name, message)
     
-#    print lan_address
-#    print IP_address
-    
-def visit(url):
+   
+def visit(url):                             #没啥说的
     opener = urllib2.urlopen(url)
     if url == opener.geturl():
         str = opener.read()
     return re.search('\d+\.\d+\.\d+\.\d+',str).group(0)
     
 if __name__ == "__main__":
-    getIP()
+    check_network()
